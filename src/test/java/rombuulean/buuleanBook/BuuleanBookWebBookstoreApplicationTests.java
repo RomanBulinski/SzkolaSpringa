0598025211a86827cd24941f16a3bac551777dc5
bulir <aboandrus@gamil.com>
2021-02-11 21:13:44 +0100

    Dodanie do gita kolejnych plików

---
+++ src/test/java/rombuulean/buuleanBook/BuuleanBookWebBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package rombuulean.buuleanBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuuleanBookWebBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
