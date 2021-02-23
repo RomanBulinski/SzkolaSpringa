9604d8dee588a2577f92bd262c29687c9817e159
bulir <aboandrus@gamil.com>
2021-02-23 20:03:58 +0100

    Modul4_Lekcja6: Odbieranie body, metoda Post

--- src/main/java/rombuulean/buuleanBook/catalog/web/CatalogController.java
+++ src/main/java/rombuulean/buuleanBook/catalog/web/CatalogController.java
@@ -1,12 +1,17 @@
 package rombuulean.buuleanBook.catalog.web;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import rombuulean.buuleanBook.catalog.application.port.CatalogUseCase;
+import rombuulean.buuleanBook.catalog.application.port.CatalogUseCase.CreateBookCommand;
 import rombuulean.buuleanBook.catalog.domain.Book;
 
+import java.math.BigDecimal;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -54,5 +59,32 @@ public class CatalogController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+//    public Book addBook(@RequestBody  RestCreateBookCommand command){
+    public ResponseEntity<Void> addBook(@RequestBody  RestCreateBookCommand command){
+        Book book =catalog.addBook(command.toCommand());
+        return ResponseEntity.created(createdBookuri(book)).build();
+    }
+
+    private URI createdBookuri(Book book) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/"+ book.getId().toString()).build().toUri();
+        return uri;
+    }
+
+    @Data
+    private static class RestCreateBookCommand {
+        private String title;
+        private String author;
+        private Integer year;
+        private BigDecimal price;
+
+        CreateBookCommand toCommand(){
+            return new CreateBookCommand(title, author, year, price);
+        }
+
+    }
+
 
 }
