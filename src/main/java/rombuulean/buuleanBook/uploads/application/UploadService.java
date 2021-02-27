411228901e69c5acb7612309c3def0ea0ec93c53
bulir <aboandrus@gamil.com>
2021-02-27 10:55:41 +0100

    Modul4_Lekcja13: Serwowanie plikow

--- src/main/java/rombuulean/buuleanBook/uploads/application/UploadService.java
+++ src/main/java/rombuulean/buuleanBook/uploads/application/UploadService.java
@@ -6,7 +6,9 @@ import rombuulean.buuleanBook.uploads.application.port.UploadUseCase;
 import rombuulean.buuleanBook.uploads.domain.Upload;
 
 import java.time.LocalDateTime;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
@@ -16,7 +18,7 @@ public class UploadService implements UploadUseCase {
 
     @Override
     public Upload save(SaveUploadCommand command) {
-        String newId = RandomStringUtils.randomAlphanumeric(8);
+        String newId = RandomStringUtils.randomAlphanumeric(8).toLowerCase();
         Upload upload = new Upload(
                 newId,
                 command.getFile(),
@@ -27,6 +29,14 @@ public class UploadService implements UploadUseCase {
         storage.put(upload.getId(),upload );
         System.out.println("Upload saved : " + upload.getFilename() + " with id: " + newId);
         return upload;
+    }
+
+    @Override
+    public Optional<Upload> getById(String id) {
+        return Optional.ofNullable(storage.get(id));
+
+
+
     }
 
 }
