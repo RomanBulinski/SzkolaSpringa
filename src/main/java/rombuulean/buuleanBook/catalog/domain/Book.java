c6477c317ef2add30fd20bbfde925f3592aa47fa
bulir <aboandrus@gamil.com>
2021-03-28 09:53:02 +0200

    Modul7_Lekcja12: unikalne pozycje

--- src/main/java/rombuulean/buuleanBook/catalog/domain/Book.java
+++ src/main/java/rombuulean/buuleanBook/catalog/domain/Book.java
@@ -24,6 +24,7 @@ import java.util.Set;
 @Entity
 public class Book extends BaseEntity {
 
+    @Column(unique = true)
     private String title;
     private Integer year;
     private BigDecimal price;
