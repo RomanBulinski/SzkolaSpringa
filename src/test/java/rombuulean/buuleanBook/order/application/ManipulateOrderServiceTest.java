06a9763725a5791907ed6887129e39367fc8b344
bulir <aboandrus@gamil.com>
2021-04-02 20:05:57 +0200

    Modul8_Lekcja7: Testowanie wyjątków

--- src/test/java/rombuulean/buuleanBook/order/application/ManipulateOrderServiceTest.java
+++ src/test/java/rombuulean/buuleanBook/order/application/ManipulateOrderServiceTest.java
@@ -1,18 +1,20 @@
 package rombuulean.buuleanBook.order.application;
 
-import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import rombuulean.buuleanBook.catalog.db.BookJpaRepository;
 import rombuulean.buuleanBook.catalog.domain.Book;
-import rombuulean.buuleanBook.order.application.port.ManipulateOrderUseCase;
+import rombuulean.buuleanBook.order.application.port.ManipulateOrderUseCase.PlaceOrderResponse;
 import rombuulean.buuleanBook.order.domain.Recipient;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static rombuulean.buuleanBook.order.application.port.ManipulateOrderUseCase.OrderItemCommand;
+import static rombuulean.buuleanBook.order.application.port.ManipulateOrderUseCase.PlaceOrderCommand;
 
 @DataJpaTest
 @Import({ManipulateOrderService.class})
@@ -27,31 +29,51 @@ class ManipulateOrderServiceTest {
     public void userCanPlaceOrder() {
 
         //given
-        Book  effectiveJava = givenJavaConcurrency(50);
-        Book  jcip = givenEffectiveJava(50);
-        ManipulateOrderUseCase.PlaceOrderCommand command = ManipulateOrderUseCase.PlaceOrderCommand
+        Book effectiveJava = givenJavaConcurrency(50);
+        Book jcip = givenEffectiveJava(50);
+        PlaceOrderCommand command = PlaceOrderCommand
                 .builder()
-                .recipient(recipient() )
-                .item( new ManipulateOrderUseCase.OrderItemCommand(effectiveJava.getId(), 10))
-                .item( new ManipulateOrderUseCase.OrderItemCommand(jcip.getId(), 10))
+                .recipient(recipient())
+                .item(new OrderItemCommand(effectiveJava.getId(), 10))
+                .item(new OrderItemCommand(jcip.getId(), 10))
                 .build();
         //when
-        ManipulateOrderUseCase.PlaceOrderResponse response = service.placeOrder(command);
+        PlaceOrderResponse response = service.placeOrder(command);
         //then
         assertTrue(response.isSuccess());
 
     }
 
+    @Test
+    public void userCantOrderMoreBooksThanAvailable() {
 
-    private Book givenJavaConcurrency(long available){
-        return bookJpaRepository.save(new Book("Java Concurrency in Practice",2006, new BigDecimal("90.90"),available));
+        //given
+        Book effectiveJava = givenJavaConcurrency(5);
+        Book jcip = givenEffectiveJava(50);
+        PlaceOrderCommand command = PlaceOrderCommand
+                .builder()
+                .recipient(recipient())
+                .item(new OrderItemCommand(effectiveJava.getId(), 10))
+                .item(new OrderItemCommand(jcip.getId(), 10))
+                .build();
+        //when
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            service.placeOrder(command);
+        });
+        //then
+        assertTrue(exception.getMessage().contains("Too many copies of book " + effectiveJava.getId() + " requested: 10 of 5 available"));
     }
 
-    private Book givenEffectiveJava(long available){
-        return bookJpaRepository.save(new Book("Effective Java",2006, new BigDecimal("90.90"),available));
+
+    private Book givenJavaConcurrency(long available) {
+        return bookJpaRepository.save(new Book("Java Concurrency in Practice", 2006, new BigDecimal("90.90"), available));
     }
 
-    private Recipient recipient(){
+    private Book givenEffectiveJava(long available) {
+        return bookJpaRepository.save(new Book("Effective Java", 2006, new BigDecimal("90.90"), available));
+    }
+
+    private Recipient recipient() {
         return Recipient.builder().email("jhon@example.org").build();
     }
 }
